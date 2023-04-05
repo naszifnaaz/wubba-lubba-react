3eaeda57c42247f16dbe8bbf0207557f0c79f3ed
naszifnaaz <naszifnaaz@gmail.com>
2023-04-05 22:44:30 +0530

    fix : broken-links

--- src/components/Branding.jsx
+++ src/components/Branding.jsx
@@ -1,9 +1,10 @@
 import { Image } from "@chakra-ui/react";
+import logo from "../assets/rick-and-morty-logo.png";
 
 export function Branding() {
   return (
     <Image
-      src={"https://assets.stickpng.com/images/58f37720a4fa116215a9240f.png"}
+      src={logo}
       alt="Rick And Morty"
       margin={"auto auto"}
       width={"400px"}
